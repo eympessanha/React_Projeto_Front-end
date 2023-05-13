20f64a9c86009d9f9be540fdfc7b7f8459e8e0ca
HugoCorrea01 <hcalixto2812@gmail.com>
2023-05-12 23:15:29 -0300

    commit

---
+++ src/components/Menu/style.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+const MeuNav = styled.nav`
+    display: flex;
+    flex-direction: row;
+    gap: 2em;
+    a {
+        text-decoration: none;
+    }
+`;
+
+export { MeuNav };
