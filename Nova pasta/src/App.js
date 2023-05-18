f1d823ea40d26192b130bec003d79f2e3523acc4
LiporaceM <108703224+LiporaceM@users.noreply.github.com>
2023-05-17 21:28:49 -0300

    Add files via upload

---
+++ Nova pasta/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Navbar from "./Components/Navbar.jsx";
+
+function App() {
+	return (
+		<React.Fragment>
+			<Navbar/>
+		</React.Fragment>
+	);
+}
+
+export default App;
