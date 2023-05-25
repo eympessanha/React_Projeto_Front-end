eb4073395315e1dfcc04063a837c5c7894b7d988
HugoCorrea01 <127753509+HugoCorrea01@users.noreply.github.com>
2023-05-25 02:09:36 -0300

    App.jsx

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -3,17 +3,18 @@ import Menu from '../components/Menu/Menu';
 import as3 from '../img/as3.svg'
 function App(){
   return (
-    <><Menu /> 
-    <section className='app'>
-      <h1>
-        Bem-vindo ao <span>AS3</span>
-      </h1>
+    <><Menu /><div className='App'>
+      <section className='app'>
+        <h1>
+          Bem-vindo ao <span>AS3</span>
+        </h1>
         <p>Comece a criar seus chamados agora mesmo! </p>
-        <a href="./app.jxs">Criar Chamados</a>
-      <img src={as3} alt='AS3'/>
+        <a href="/">Criar Chamados</a>
+        <img src={as3} alt='AS3' />
 
 
-    </section></>
+      </section>
+    </div></>
   )
 }
   
