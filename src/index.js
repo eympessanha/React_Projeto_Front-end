b0be1291b293846c790163585a7ee64b5bb91ea4
eympessanha <126716859+eympessanha@users.noreply.github.com>
2023-06-01 10:42:35 -0300

    Update index.js
    update

--- src/index.js
+++ src/index.js
@@ -6,13 +6,10 @@ import reportWebVitals from './reportWebVitals';
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import Login from './pages/Login';
 import Cadastro from './pages/Cadastro';
-<<<<<<< HEAD
 import AberturaTicket from './pages/AberturaTicket';
-=======
 import Recuperar from './pages/Recuperar';
 import Chamados_externo from './pages/Chamados_externos';
 
->>>>>>> 97fbda0f7750162d097679556daf190f86c2c02b
 
 const rotas = createBrowserRouter([
   {
@@ -28,17 +25,15 @@ const rotas = createBrowserRouter([
     element: <Cadastro />
   },
   {
-<<<<<<< HEAD
     path: "AberturaTicket",
-    element: <AberturaTicket />
-=======
+    element: <AberturaTicket />},
+  {
     path: "recuperar",
     element: <Recuperar />
   },
   {
     path: "Chamado_externo",
     element: <Chamados_externo/>
->>>>>>> 97fbda0f7750162d097679556daf190f86c2c02b
   }
 ])
 
