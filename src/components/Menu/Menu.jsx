99bbeb0e1e010dc54a3315e9556b4f8c971ccac1
HUGO CORREA CALIXTO REGO FARRANHA <202301134391@alunos.ibmec.edu.br>
2023-06-01 09:17:22 -0300

    menu

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -1,12 +1,21 @@
 import { Link } from "react-router-dom";
 import { MeuNav } from "./style";
+import "./App.css"
+import logo from "./logoas3.png"
 
-
-const Menu = () => (
-    <MeuNav>
-        <Link to="/">Home</Link>
-        <Link to="/login">Login</Link>
-    </MeuNav>
-);
+const Menu = () => {
+  return (
+    <nav className="menu"> 
+      <ul>
+        <li><a href="#"><img src="{logo}" alt="logo"/></a></li>
+        <li><a href="#">Home</a></li>
+        <li><a href="#">Dashboard</a></li>
+        <li><a href="#">Chamados</a></li>
+        <li><a href="#">Login</a></li>
+      </ul>
+    </nav>
+  );
+};
 
 export default Menu;
+
